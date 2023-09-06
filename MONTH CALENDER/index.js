ccbf27ad199ec8296d07b9da4f11fe8d7c70b6a4
Sami4code <hprsamuel@gmail.com>
2023-09-06 10:34:14 +0100

    JS part of the month-calender

--- MONTH CALENDER/index.js
+++ MONTH CALENDER/index.js
@@ -4,8 +4,9 @@ const daysEl = document.querySelector(".days"); /* selects p from html*/
 
 
 const monthInx = new Date().getMonth();
-const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
-const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1
+const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate(); /* zero means the previous month, 1 means the current month, 
+if 2, it means the following month... .getDate gets last date which is nummber 30 */ 
+const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1;
 console.log(firstDay);
 
 const months = [
@@ -29,7 +30,7 @@ fullDateEl.innerText = new Date().toDateString(); /* displays the p of the calen
 let days = "";
 
 for(let i = firstDay; i > 0; i--) {
-    days += `<div class="empty">${i}</div>`;
+    days += `<div class="empty"></div>`; /* removes the numbers that occupied the empty divs, and leaves them empty */
 }
 
 for (let i = 1; i <= lastDay; i++) {
